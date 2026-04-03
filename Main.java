7a42c0a19814823bb54782b11897a1a42e78ca00
chandan <chandanlokesh318@gmail.com>
2026-04-03 19:12:53 +0530

    added .gitignore

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Every Java program must be inside a class
+public class Main {
+    // The main method is the entry point of any Java application
+    public static void main(String[] args) {
+        // This line prints text to the console
+        System.out.println("Hello, World!");
+    }
+}
+
+
